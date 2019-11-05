da9046aa88e09759620a25302fa8f6a69bb4f8f5
Hao Wu <wuh@microsoft.com>
2019-11-05 22:33:22 +0800

    Simplify and improve.

--- mp3gen/mp3gen.py
+++ mp3gen/mp3gen.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import yaml
 import hashlib
 
 #
 
-input_dir = "origin"
+spec_file_path = sys.argv[1]
+input_dir = os.path.dirname(spec_file_path)
 tmp_dir = "_tmp"
 
 cmdseq = []
@@ -14,57 +16,34 @@ parts = []
 
 #
 
-with open(os.path.join(input_dir, "spec.yaml"), "r", encoding="utf-8") as f_in:
+with open(spec_file_path, "r", encoding="utf-8") as f_in:
     spec = yaml.load(f_in, Loader=yaml.SafeLoader)
 
 #
 
 for x in spec["sequence"]:
+    print(x)
+    cmd_prefix = ""
     if "fileName" in x:
-        file_path = os.path.join(input_dir, x["fileName"])
-        ext = os.path.splitext(file_path)[1].lower()
-        if ext == ".m4a":
-            cmd_prefix = "ffmpeg -y -i \"%s\" -codec:v copy -codec:a libmp3lame -q:a 4" % file_path
-            cmd_sig = hashlib.md5(cmd_prefix.encode('utf-8')).hexdigest()
-            file_path = os.path.join(tmp_dir, cmd_sig + ".mp3")
-            cmd = "%s \"%s\"" % (cmd_prefix, file_path)
-            cmdseq.append({
-                "cmd_sig": cmd_sig,
-                "cmd": cmd,
-                "excluded": True
-            })
+        input_file_path = os.path.join(input_dir, x["fileName"])
         begin = "" if "begin" not in x else " -ss %s" % x["begin"]
         end = "" if "end" not in x else " -to %s" % x["end"]
-        cmd_prefix = "ffmpeg -y -i \"%s\"%s%s -acodec copy" % (file_path, begin, end)
-        cmd_sig = hashlib.md5(cmd_prefix.encode('utf-8')).hexdigest()
-        file_path = os.path.join(tmp_dir, cmd_sig + ".mp3")
-        cmd = "%s \"%s\"" % (cmd_prefix, file_path)
-        cmdseq.append({
-            "cmd_sig": cmd_sig,
-            "cmd": cmd
-        })
-        parts.append(file_path)
+        cmd_prefix = "ffmpeg -y -i \"%s\"%s%s" % (input_file_path, begin, end)
+        ext = os.path.splitext(input_file_path)[1].lower()
+        if ext == ".m4a":
+            cmd_prefix += " -codec:v copy -codec:a libmp3lame -q:a 4"
+        elif ext == ".mp3":
+            cmd_prefix += " -acodec copy"
     elif "silenceInSeconds" in x:
         cmd_prefix = "ffmpeg -y -f lavfi -i anullsrc=channel_layout=5.1:sample_rate=48000 -t %d" % x["silenceInSeconds"]
-        cmd_sig = hashlib.md5(cmd_prefix.encode('utf-8')).hexdigest()
-        file_path = os.path.join(tmp_dir, cmd_sig + ".mp3")
-        cmd = "%s \"%s\"" % (cmd_prefix, file_path)
-        cmdseq.append({
-            "cmd_sig": cmd_sig,
-            "cmd": cmd
-        })
-        parts.append(file_path)
-
-#
-
-cmd_prefix = "ffmpeg -y -i \"concat:%s\" -codec:a libmp3lame -q:a 4" % "|".join(parts)
-cmd_sig = hashlib.md5(cmd_prefix.encode('utf-8')).hexdigest()
-file_path = spec["name"] + ".mp3"
-cmd = "%s \"%s\"" % (cmd_prefix, file_path)
-cmdseq.append({
-    "cmd_sig": cmd_sig,
-    "cmd": cmd
-})
+    cmd_sig = hashlib.md5(cmd_prefix.encode('utf-8')).hexdigest()
+    output_file_path = os.path.join(tmp_dir, cmd_sig + ".mp3")
+    cmd = "%s \"%s\"" % (cmd_prefix, output_file_path)
+    cmdseq.append({
+        "cmd": cmd,
+        "cmd_sig": cmd_sig,
+        "output_file_path": output_file_path
+    })
 
 #
 
@@ -75,6 +54,19 @@ for x in cmdseq:
     if not os.path.isfile(os.path.join(tmp_dir, x["cmd_sig"] + ".mp3")):
         print(x["cmd"])
         os.system(x["cmd"])
+
+#
+
+parts = []
+for x in cmdseq:
+    parts.append(x["output_file_path"])
+
+print(parts)
+
+cmd = "ffmpeg -y -i \"concat:%s\" -codec:a libmp3lame -q:a 4 %s" \
+    % ("|".join(parts), spec["name"] + ".mp3")
+print(cmd)
+os.system(cmd)
 
 #
 # (END)
